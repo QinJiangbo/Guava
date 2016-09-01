151010df978709d343ed2d6a1f94ca6065fd08d3
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-02 00:47:14 +0800

    add new ArrayList constructor

--- src/main/java/com/qinjiangbo/Functions.java
+++ src/main/java/com/qinjiangbo/Functions.java
@@ -1,8 +1,13 @@
 package com.qinjiangbo;
 
+import com.google.common.collect.Lists;
+
 /**
  * Created by Richard on 9/1/16.
  */
 public class Functions {
 
+    public void lists() {
+        Lists.newArrayList();
+    }
 }

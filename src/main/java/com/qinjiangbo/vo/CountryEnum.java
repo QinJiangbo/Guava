ca75a2ed2f59711535bcb7a82cdc031f37672a28
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-06 03:21:56 +0800

    predicate compositions

--- src/main/java/com/qinjiangbo/vo/CountryEnum.java
+++ src/main/java/com/qinjiangbo/vo/CountryEnum.java
@@ -1,26 +1,43 @@
 package com.qinjiangbo.vo;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 /**
  * Date: 9/4/16
  * Author: qinjiangbo@github.io
  */
 public enum CountryEnum {
-    CHINA("CHINA", "BEIJING", 2000),
-    US("US", "WASHINGTON DC", 200),
-    KOREA("KOREA", "SEUL", 500),
-    GB("GB", "LONDON", 1000),
-    FINLAND("FINLAND", "", 1000);
+    CHINA("CHINA", "BEIJING", 2000, "ASIA"),
+    US("US", "WASHINGTON DC", 200, "AMERICA"),
+    KOREA("KOREA", "SEUL", 500, "ASIA"),
+    GB("GB", "LONDON", 1000, "EURO"),
+    FINLAND("FINLAND", "", 1000, "EURO");
 
     private String name;
     private String capital;
     private int age;
+    private String continent;
 
-    private CountryEnum(String name, String capital, int age) {
+    private CountryEnum(String name, String capital, int age, String continent) {
         this.name = name;
         this.capital = capital;
         this.age = age;
+        this.continent = continent;
     }
 
+    public static List<CountryEnum> findCountries() {
+        return Lists.newArrayList(CHINA, US, KOREA, FINLAND, GB);
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
 
     public String getName() {
         return name;
--- src/main/test/com/qinjiangbo/PredicateTest.java
+++ src/main/test/com/qinjiangbo/PredicateTest.java
@@ -1,12 +1,15 @@
 package com.qinjiangbo;
 
 import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.google.common.base.Strings;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.qinjiangbo.vo.CountryEnum;
 import com.sun.istack.internal.Nullable;
 import org.junit.Test;
+
+import java.util.Iterator;
 
 /**
  * Date: 9/5/16
@@ -32,6 +35,28 @@ public class PredicateTest {
 
     @Test
     public void testComposedPredicate() {
+        Predicate<CountryEnum> fromAsiaPredicate = new Predicate<CountryEnum>() {
+            @Override
+            public boolean apply(@Nullable CountryEnum country) {
+                return country.getContinent().equals("ASIA");
+            }
+        };
 
+        Predicate<CountryEnum> historyPredicate = new Predicate<CountryEnum>() {
+            @Override
+            public boolean apply(@Nullable CountryEnum country) {
+                return country.getAge() > 1000;
+            }
+        };
+
+        Predicate<CountryEnum> composedPredicate = Predicates.and(fromAsiaPredicate, historyPredicate);
+
+        Iterable<CountryEnum> filteredCountries = Iterables.filter(CountryEnum.findCountries(), composedPredicate);
+
+        Iterator iterator = filteredCountries.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
+
 }

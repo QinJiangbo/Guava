519c621db34b5825ab824575f5e0d552ce97a7bc
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-10 22:48:47 +0800

    split using CharMatcher

--- src/main/test/com/qinjiangbo/SplitterTest.java
+++ src/main/test/com/qinjiangbo/SplitterTest.java
@@ -1,5 +1,6 @@
 package com.qinjiangbo;
 
+import com.google.common.base.CharMatcher;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import org.junit.Test;
@@ -21,8 +22,16 @@ public class SplitterTest {
 
     @Test
     public void testSplitOnRegExp() {
-        Iterable<String> iterable = Splitter.onPattern("\\d+").split("Java3Scala4Php5C#6");
+        Iterable<String> iterable = Splitter.onPattern("\\d+").split("Java13Scala41Php5C#6");
         List<String> splittedList = Lists.newArrayList(iterable);
         System.out.println(splittedList.get(2).equals("Php"));
     }
+
+    @Test
+    public void testSplitUsingCharMatcher() {
+        Iterable<String> iterable = Splitter.on(CharMatcher.inRange('3', '8')).split("Java3Scala4Haskell7Brain9Kotlin");
+        List<String> splittedList = Lists.newArrayList(iterable);
+        System.out.println(splittedList.get(2));
+    }
+
 }

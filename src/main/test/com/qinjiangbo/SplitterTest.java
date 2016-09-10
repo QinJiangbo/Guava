921b2af1a9cd58aca2abbbd40a92ed367d54d204
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-10 21:38:57 +0800

    split on semicolons

--- src/main/test/com/qinjiangbo/SplitterTest.java
+++ src/main/test/com/qinjiangbo/SplitterTest.java
@@ -1,9 +1,21 @@
 package com.qinjiangbo;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.List;
+
 /**
  * Date: 9/10/16
  * Author: qinjiangbo@github.io
  */
 public class SplitterTest {
 
+    @Test
+    public void testSplitOnSemicolons() {
+        Iterable<String> iterable = Splitter.on(";").split("Java;Scala;Php;Haskell");
+        List<String> splittedList = Lists.newArrayList(iterable);
+        System.out.println(splittedList.get(2).equals("Php"));
+    }
 }

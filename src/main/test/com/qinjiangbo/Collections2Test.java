f1b4cc61a05cf1e3fe370f0e744db32d035a9708
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-14 23:55:43 +0800

    ccreate collections2 test cases

--- src/main/test/com/qinjiangbo/Collections2Test.java
+++ src/main/test/com/qinjiangbo/Collections2Test.java
@@ -1,6 +1,8 @@
 package com.qinjiangbo;
 
 import com.google.common.base.Function;
+import com.google.common.base.Objects;
+import com.google.common.base.Predicate;
 import com.google.common.base.Strings;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
@@ -38,5 +40,53 @@ public class Collections2Test {
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
+    }
+
+    @Test
+    public void testFilterCollections() {
+        ArrayList<CountryEnum> countries = Lists.newArrayList(CountryEnum.CHINA, CountryEnum.FINLAND, CountryEnum.KOREA);
+        Collection<CountryEnum> countriesFromAsia = Collections2.filter(countries, new Predicate<CountryEnum>() {
+            @Override
+            public boolean apply(@Nullable CountryEnum country) {
+                return Objects.equal("ASIA", country.getContinent());
+            }
+        });
+
+        Iterator<CountryEnum> iterator = countriesFromAsia.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+    }
+
+    @Test
+    public void testShowThatResultIsOnlyAView() {
+        ArrayList<CountryEnum> countries = Lists.newArrayList(CountryEnum.CHINA, CountryEnum.FINLAND, CountryEnum.KOREA);
+
+        Collection<String> capitals = Collections2.transform(countries, new Function<CountryEnum, String>() {
+            @Override
+            public String apply(@Nullable CountryEnum country) {
+                return Strings.isNullOrEmpty(country.getCapital()) ? "UNKNOWN" : country.getCapital();
+            }
+
+            @Override
+            public boolean equals(Object object) {
+                return false;
+            }
+        });
+
+        Iterator<String> iterator = capitals.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("=======================");
+        countries.add(CountryEnum.US);
+
+        iterator = capitals.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
     }
 }

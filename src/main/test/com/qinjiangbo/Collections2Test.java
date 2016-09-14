09084b591388e89ffb7fc537b8f1e77345177733
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-14 23:23:26 +0800

    ccreate collections2 test cases

---
+++ src/main/test/com/qinjiangbo/Collections2Test.java
@@ -0,0 +1,42 @@
+package com.qinjiangbo;
+
+import com.google.common.base.Function;
+import com.google.common.base.Strings;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+import com.qinjiangbo.vo.CountryEnum;
+import com.sun.istack.internal.Nullable;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Date: 9/14/16
+ * Author: qinjiangbo@github.io
+ */
+public class Collections2Test {
+
+    @Test
+    public void testTransformCollection() {
+        ArrayList<CountryEnum> countries = Lists.newArrayList(CountryEnum.CHINA, CountryEnum.FINLAND, CountryEnum.KOREA);
+
+        Collection<String> capitals = Collections2.transform(countries, new Function<CountryEnum, String>() {
+            @Override
+            public String apply(@Nullable CountryEnum country) {
+                return Strings.isNullOrEmpty(country.getCapital()) ? "UNKNOWN" : country.getCapital();
+            }
+
+            @Override
+            public boolean equals(Object object) {
+                return false;
+            }
+        });
+
+        Iterator<String> iterator = capitals.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}

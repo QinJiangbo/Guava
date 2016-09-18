bc2edce50097b60f415a81eca96b668cb8b4e55e
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-19 00:11:49 +0800

    create Iterables class

--- src/main/test/com/qinjiangbo/IterablesTest.java
+++ src/main/test/com/qinjiangbo/IterablesTest.java
@@ -1,10 +1,13 @@
 package com.qinjiangbo;
 
 import com.google.common.base.Predicate;
+import com.google.common.base.Strings;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.sun.istack.internal.Nullable;
 import org.junit.Test;
+
+import java.util.Iterator;
 
 /**
  * Date: 9/18/16
@@ -26,5 +29,28 @@ public class IterablesTest {
         };
 
         System.out.println(Iterables.all(Lists.newArrayList("Hello", "Java", "Haskell", "Python"), lengthPredicate));
+    }
+
+    @Test
+    public void testIterablesAny() {
+
+        Predicate<String> emptyOrNullPredicate = new Predicate<String>() {
+            @Override
+            public boolean apply(String input) {
+                return Strings.isNullOrEmpty(input);
+            }
+        };
+
+        System.out.println(Iterables.any(Lists.newArrayList("Java", "Java", "Kotlin"), emptyOrNullPredicate));
+    }
+
+    @Test
+    public void testCycleOverIterable() {
+        Iterable<String> cycleIterables = Iterables.cycle("Right", "Left");
+        Iterator<String> iterator = cycleIterables.iterator();
+        for (int i = 0; i < 100; i++) {
+            iterator.next();
+        }
+        System.out.println(iterator.next());
     }
 }

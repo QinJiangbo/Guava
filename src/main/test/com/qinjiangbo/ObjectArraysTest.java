a4856a7398d614b81f32e892fdb3e7c071cd1249
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-19 23:41:46 +0800

    create ObjectArrays class

--- src/main/test/com/qinjiangbo/ObjectArraysTest.java
+++ src/main/test/com/qinjiangbo/ObjectArraysTest.java
@@ -1,9 +1,20 @@
 package com.qinjiangbo;
 
+import com.google.common.collect.ObjectArrays;
+import org.junit.Test;
+
 /**
  * Date: 9/19/16
  * Author: qinjiangbo@github.io
  */
 public class ObjectArraysTest {
 
+    @Test
+    public void concatTwoArrays() {
+        String[] array1 = new String[]{"one", "two", "three"};
+        String[] array2 = new String[]{"four", "five"};
+        String[] array3 = ObjectArrays.concat(array1, array2, String.class);
+        System.out.println(array3.length);
+        System.out.println("array3[2] => " + array3[2]);
+    }
 }

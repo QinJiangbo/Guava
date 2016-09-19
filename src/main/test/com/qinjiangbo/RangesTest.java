13aa54ae6c7943340c2ed0d6cd81127f3e060ebb
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-20 00:00:51 +0800

    create Range class

--- src/main/test/com/qinjiangbo/RangesTest.java
+++ src/main/test/com/qinjiangbo/RangesTest.java
@@ -1,5 +1,6 @@
 package com.qinjiangbo;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Range;
 import org.junit.Test;
 
@@ -16,4 +17,23 @@ public class RangesTest {
         System.out.println(range.contains(20));
         System.out.println(rangeWithRightOpen.contains(20));
     }
+
+    @Test
+    public void testCheckThatRangeIsEnclosedInOtherRange() {
+        Range<Long> range = Range.openClosed(2L, 20L); // (2, 20]
+        Range<Long> smallerRange = Range.closed(1L, 15L); // [1, 15]
+        System.out.println(range.encloses(smallerRange));
+        Range newRange = range.span(smallerRange);
+        System.out.println(newRange);
+        Range newRange2 = range.intersection(smallerRange);
+        System.out.println(newRange2);
+    }
+
+    @Test
+    public void testCheckThatAllElementIsInRange() {
+        Range<Integer> range = Range.closed(2, 20);
+        System.out.println(range.containsAll(Lists.newArrayList(10, 20, 18, 8, 5, 19)));
+    }
+
+
 }

85dd0966779a514d94692cf36b8a2cf333f41183
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-19 22:59:00 +0800

    create MultiSet class

--- src/main/test/com/qinjiangbo/MultiSetTest.java
+++ src/main/test/com/qinjiangbo/MultiSetTest.java
@@ -1,8 +1,20 @@
 package com.qinjiangbo;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import org.junit.Test;
+
 /**
  * Date: 9/19/16
  * Author: qinjiangbo@github.io
  */
 public class MultiSetTest {
+
+    @Test
+    public void testAddElementsTwoTimes() {
+        Multiset<String> multiset = HashMultiset.create();
+        multiset.add("one");
+        multiset.add("one");
+        System.out.println(multiset.count("one"));
+    }
 }

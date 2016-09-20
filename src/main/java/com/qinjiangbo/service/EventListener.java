011d9b215946bfacc38dc1aba807cb994586831b
QinJiangbo <qinjiangbo@users.noreply.github.com>
2016-09-20 23:31:59 +0800

    create EventBus class

--- src/main/java/com/qinjiangbo/service/EventListener.java
+++ src/main/java/com/qinjiangbo/service/EventListener.java
@@ -1,5 +1,7 @@
 package com.qinjiangbo.service;
 
+import com.google.common.eventbus.Subscribe;
+
 /**
  * Date: 9/20/16
  * Author: qinjiangbo@github.io
@@ -8,6 +10,7 @@ public class EventListener {
 
     public int lastMessage = 0;
 
+    @Subscribe
     public void listen(OurTestEvent event) {
         lastMessage = event.getMessage();
     }
--- src/main/test/com/qinjiangbo/EventBusTest.java
+++ src/main/test/com/qinjiangbo/EventBusTest.java
@@ -1,8 +1,22 @@
 package com.qinjiangbo;
 
+import com.google.common.eventbus.EventBus;
+import com.qinjiangbo.service.EventListener;
+import com.qinjiangbo.service.OurTestEvent;
+import org.junit.Test;
+
 /**
  * Date: 9/20/16
  * Author: qinjiangbo@github.io
  */
 public class EventBusTest {
+
+    @Test
+    public void testReceiveEvent() {
+        EventBus eventBus = new EventBus("test");
+        EventListener eventListener = new EventListener();
+        eventBus.register(eventListener);
+        eventBus.post(new OurTestEvent(200));
+        System.out.println(eventListener.getLastMessage());
+    }
 }

15270c85f5a84a0ace6738934a14c77d036d9e0b
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-21 22:33:19 +0800

    create EventBus class

--- src/main/test/com/qinjiangbo/MathTest.java
+++ src/main/test/com/qinjiangbo/MathTest.java
@@ -1,5 +1,6 @@
 package com.qinjiangbo;
 
+import com.google.common.math.DoubleMath;
 import com.google.common.math.IntMath;
 import org.junit.Test;
 
@@ -13,6 +14,17 @@ public class MathTest {
 
     @Test
     public void testSqrt() {
-        System.out.println(IntMath.sqrt(17, RoundingMode.DOWN));
+        System.out.println(IntMath.sqrt(26, RoundingMode.DOWN));
     }
+
+    @Test
+    public void testFactorial() {
+        System.out.println(DoubleMath.factorial(10));
+    }
+
+    @Test
+    public void testMean() {
+        System.out.println(DoubleMath.mean(10, 276, 89, 46, 59));
+    }
+
 }

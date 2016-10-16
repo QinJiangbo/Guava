00bca0c85d42e2b169881d8c75a002673405b65c
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-10-16 16:28:30 +0800

    add test for Table and Cache

--- src/main/test/com/qinjiangbo/TableTest.java
+++ src/main/test/com/qinjiangbo/TableTest.java
@@ -1,8 +1,33 @@
 package com.qinjiangbo;
 
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import org.junit.Test;
+
 /**
  * Date: 16/10/2016
  * Author: qinjiangbo@github.io
  */
 public class TableTest {
+
+    @Test
+    public void testTable() {
+        Table<String, Integer, String> table = HashBasedTable.create();
+
+        for (char a = 'A'; a <= 'C'; ++a) {
+            for (Integer b = 1; b <= 3; ++b) {
+                table.put(Character.toString(a), b, String.format("%c%d", a, b));
+            }
+        }
+
+        System.out.println(table.column(2));
+        System.out.println(table.row("B"));
+        System.out.println(table.get("B", 2));
+
+        System.out.println(table.contains("D", 1));
+        System.out.println(table.containsColumn(3));
+        System.out.println(table.containsRow("C"));
+        System.out.println(table.columnMap());
+        System.out.println(table.rowMap());
+    }
 }

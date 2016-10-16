b2e3c55f473b5600d4990b9a0debc55a3d097fe9
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-10-17 00:33:35 +0800

    add Cache Test Cases

--- src/main/test/com/qinjiangbo/CacheTest.java
+++ src/main/test/com/qinjiangbo/CacheTest.java
@@ -1,9 +1,26 @@
 package com.qinjiangbo;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.junit.Test;
+
 /**
  * Date: 16/10/2016
  * Author: qinjiangbo@github.io
  */
 public class CacheTest {
 
+    @Test
+    public void testLoadingCache() {
+        LoadingCache<String, String> cache = CacheBuilder
+                .newBuilder()
+                .build(new CacheLoader<String, String>() {
+                    @Override
+                    public String load(String key) throws Exception {
+                        return null;
+                    }
+                });
+
+    }
 }

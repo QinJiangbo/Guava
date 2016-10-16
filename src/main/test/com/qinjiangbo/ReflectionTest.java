1a1f5e5be417415bf51586ff35e868b5a4e93aec
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-10-17 01:33:06 +0800

    add Relection Dynamic Test Cases

--- src/main/test/com/qinjiangbo/ReflectionTest.java
+++ src/main/test/com/qinjiangbo/ReflectionTest.java
@@ -1,10 +1,14 @@
 package com.qinjiangbo;
 
+import com.google.common.collect.Lists;
+import com.google.common.reflect.Reflection;
+import com.google.common.reflect.TypeToken;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 
 interface People {
     void printPeople();
@@ -25,14 +29,7 @@ public class ReflectionTest {
         People people = (People) Proxy.newProxyInstance(
                 People.class.getClassLoader(),
                 new Class[]{People.class},
-                new InvocationHandler() {
-                    @Override
-                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        System.out.println("method name: " + method.getName());
-                        System.out.println("args: " + (args == null ? "null" : args));
-                        return method.invoke(student, args);
-                    }
-                });
+                getHandler(student));
         people.printPeople();
     }
 
@@ -41,8 +38,35 @@ public class ReflectionTest {
      */
     @Test
     public void testGuavaProxy() {
-
+        People student = new Student("Qin Jiangbo", 23);
+        People people = Reflection.newProxy(People.class, getHandler(student));
+        people.printPeople();
     }
+
+    public InvocationHandler getHandler(Object proxiedObject) {
+        return new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                System.out.println("method name: " + method.getName());
+                System.out.println("args: " + (args == null ? "null" : args));
+                return method.invoke(proxiedObject, args);
+            }
+        };
+    }
+
+    @Test
+    public void testTypeToken() {
+        ArrayList<String> stringList = Lists.newArrayList();
+        ArrayList<Integer> intList = Lists.newArrayList();
+        // 类型被擦除了
+        System.out.println(stringList.getClass().isAssignableFrom(intList.getClass()));
+        // 利用TypeToken解析原来的类型
+        TypeToken<ArrayList<String>> typeToken = new TypeToken<ArrayList<String>>() {
+        };
+        TypeToken<?> genericType = typeToken.resolveType(ArrayList.class.getTypeParameters()[0]);
+        System.out.println(genericType.getType());
+    }
+
 }
 
 class Student implements People {

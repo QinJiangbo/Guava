8d6fae354fd42944699a761c556d89a5f5654ac3
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-10-22 21:33:06 +0800

    add ImmutableCollection Test Cases

--- src/main/test/com/qinjiangbo/ImmutCollectionTest.java
+++ src/main/test/com/qinjiangbo/ImmutCollectionTest.java
@@ -1,15 +1,20 @@
 package com.qinjiangbo;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.*;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Date: 9/18/16
  * Author: qinjiangbo@github.io
  */
 public class ImmutCollectionTest {
+
+    /**
+     * Creator or Builder of Immutable Collections
+     */
 
     @Test
     public void testImmutableMapBuilder() {
@@ -33,13 +38,13 @@ public class ImmutCollectionTest {
         ImmutableSet<String> nameSet = new ImmutableSet.Builder<String>()
                 .add("hello", "I", "am", "Richard")
                 .build();
-        System.out.println(nameSet.toString());
+        System.out.println(nameSet.toString()); // [hello, I, am, Richard]
     }
 
     @Test
     public void testImmutableSetCreator() {
         ImmutableSet<String> nameSet = ImmutableSet.of("Hello", "name", "Henry");
-        System.out.println(nameSet.toString());
+        System.out.println(nameSet.toString()); // [Hello, name, Henry]
     }
 
     @Test
@@ -47,13 +52,32 @@ public class ImmutCollectionTest {
         ImmutableList<String> nameList = new ImmutableList.Builder<String>()
                 .add("Hello", "World", "Guava", "Google")
                 .build();
-        System.out.println(nameList.toString());
+        System.out.println(nameList.toString()); // [Hello, World, Guava, Google]
     }
 
     @Test
     public void testImmutableListCreator() {
         ImmutableList<String> nameList = ImmutableList.of("Hello", "World", "Guava", "Google");
-        System.out.println(nameList);
+        System.out.println(nameList); // [Hello, World, Guava, Google]
+    }
+
+    /**
+     * copyOf
+     */
+    @Test
+    public void testImmutableCollectionCopyOf() {
+        ArrayList<String> nameList = Lists.newArrayList("Hello", "I", "like", "you", "OK");
+        ImmutableList<String> nameList2 = ImmutableList.copyOf(nameList);
+        ImmutableList<String> nameList3 = ImmutableList.copyOf(nameList.subList(1, 4));
+        System.out.println(nameList2); // [Hello, I, like, you, OK]
+        System.out.println(nameList3); // [I, like, you]
+    }
+
+    @Test
+    public void testImmutableCollectionAsList() {
+        ImmutableSet<String> nameSet = ImmutableSet.of("Hello", "Guava", "ISS", "WHU");
+        System.out.println(nameSet.asList().get(1)); // Guava
+        System.out.println(nameSet.asList()); // [Hello, Guava, ISS, WHU]
     }
 
 }

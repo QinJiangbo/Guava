ece19f06cf987a153ebb10446bb395a254176ff3
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-10-22 21:04:28 +0800

    add ImmutableCollection Test Cases

--- src/main/test/com/qinjiangbo/ImmutCollectionTest.java
+++ src/main/test/com/qinjiangbo/ImmutCollectionTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class ImmutCollectionTest {
 
     @Test
-    public void testUseMapBuilder() {
+    public void testImmutableMapBuilder() {
         ImmutableMap<String, Integer> numbersMap = new ImmutableMap.Builder<String, Integer>()
                 .put("one", 1)
                 .put("two", 2)
@@ -20,7 +20,7 @@ public class ImmutCollectionTest {
     }
 
     @Test
-    public void testUseOfCreator() {
+    public void testImmutableMapCreator() {
         ImmutableMap<String, Integer> numbersMap = ImmutableMap.of("one", 1, "two", 2,
                 "three", 3, "four", 4);
         System.out.println(numbersMap.get("one")); // 1

569c6c81e1a18caed2c0742e40b429b26a9623be
QinJiangbo <QinJiangbo@users.noreply.github.com>
2016-11-02 11:34:45 +0800

    add Hash Test Cases

--- src/main/test/com/qinjiangbo/HashTest.java
+++ src/main/test/com/qinjiangbo/HashTest.java
@@ -48,7 +48,6 @@ public class HashTest {
     @Test
     public void testGoodFastHash() {
         System.out.println(Hashing.goodFastHash(1));
-        System.out.println(Hashing.goodFastHash(10));
     }
 
     class Student {

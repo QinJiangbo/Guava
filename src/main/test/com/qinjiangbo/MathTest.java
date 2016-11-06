153d1141e6eba665e92717d4e8025868ffa2b029
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-11-07 01:37:25 +0800

    add Hash Test Cases

--- src/main/test/com/qinjiangbo/MathTest.java
+++ src/main/test/com/qinjiangbo/MathTest.java
@@ -1,9 +1,11 @@
 package com.qinjiangbo;
 
+import com.google.common.math.BigIntegerMath;
 import com.google.common.math.DoubleMath;
 import com.google.common.math.IntMath;
 import org.junit.Test;
 
+import java.math.BigInteger;
 import java.math.RoundingMode;
 
 /**
@@ -14,17 +16,47 @@ public class MathTest {
 
     @Test
     public void testSqrt() {
-        System.out.println(IntMath.sqrt(26, RoundingMode.DOWN));
+        System.out.println(IntMath.sqrt(26, RoundingMode.DOWN)); // 5
     }
 
     @Test
     public void testFactorial() {
-        System.out.println(DoubleMath.factorial(10));
+        System.out.println(DoubleMath.factorial(10)); // 3628800.0
     }
 
     @Test
     public void testMean() {
-        System.out.println(DoubleMath.mean(10, 276, 89, 46, 59));
+        System.out.println(DoubleMath.mean(10, 276, 89, 46, 59)); // 96.0
+    }
+
+    @Test
+    public void testDivide() {
+        System.out.println(IntMath.divide(10, 3, RoundingMode.UP));
+    }
+
+    @Test
+    public void testGcd() {
+        System.out.println(IntMath.gcd(60, 24)); // 12
+    }
+
+    @Test
+    public void testMod() {
+        System.out.println(IntMath.mod(192, 54)); // 30
+    }
+
+    @Test
+    public void testPow() {
+        System.out.println(IntMath.pow(2, 10)); // 1024
+    }
+
+    @Test
+    public void testIsPowerOfTwo() {
+        System.out.println(IntMath.isPowerOfTwo(36)); // false
+    }
+
+    @Test
+    public void testBinomial() {
+        System.out.println(IntMath.binomial(10, 4)); // 210 [等于C(10, 4)]
     }
 
 }

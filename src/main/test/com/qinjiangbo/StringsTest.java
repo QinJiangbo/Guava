d918e6ce142b0ded3a0eb418ba9e607a9a16cdb6
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-11-06 16:39:39 +0800

    modify cache Test Cases

--- src/main/test/com/qinjiangbo/StringsTest.java
+++ src/main/test/com/qinjiangbo/StringsTest.java
@@ -11,26 +11,32 @@ public class StringsTest {
 
     @Test
     public void testNullOrEmptyString() {
-        System.out.println(Strings.isNullOrEmpty(""));
+        System.out.println(Strings.isNullOrEmpty("")); // true
     }
 
     @Test
     public void testNullOrEmptyString2() {
-        System.out.println(Strings.isNullOrEmpty(null));
+        System.out.println(Strings.isNullOrEmpty(null)); // true
     }
 
     @Test
     public void testStringsPadEnd() {
         System.out.println(Strings.padEnd("Hello World", 20, '-'));
+        // Hello World---------
     }
 
     @Test
     public void testStringsPadStart() {
         System.out.println(Strings.padStart("Hello World", 20, '*'));
+        // *********Hello World
     }
 
     @Test
     public void testStringsRepeat() {
         System.out.println(Strings.repeat("I love you!\n", 20));
+        // I love you!
+        // I love you!
+        // ......
+        // I love you!
     }
 }
